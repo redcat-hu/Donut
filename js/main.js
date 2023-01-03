126b923ad04d24825fce49ed8b5bced2d47a83d6
Norbert N <info@red-cat.hu>
2023-01-03 20:15:20 +0100

    kapcsolat rész

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("start");
+function kuldES(form) {
+    var nev = document.getElementById("nev").value;
+    alert("Kedves " + nev + "! Üzeneted elküldtük. :)");
+    console.log("/// Kedves " + nev + "! Az email rendszer próba jellegű, inaktív");
+    return false;
+  }
+
+console.log("DONUT PARADISE | created by REDCAT")
